af246c8c06c27148c2c7f282feafc7c4b047cf44
kristen-schneider <kristen.schneider@colorado.edu>
2022-02-24 13:41:37 -0700

    quotes for boost libraries for now

--- src/gzipWrapper.cpp
+++ src/gzipWrapper.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <string>
-#include <boost/iostreams/filtering_stream.hpp>
-#include <boost/iostreams/copy.hpp>
-#include <boost/iostreams/filter/gzip.hpp>
+#include "boost/iostreams/filtering_stream.hpp"
+#include "boost/iostreams/copy.hpp"
+#include "boost/iostreams/filter/gzip.hpp"
 #include "../lib/boost_1_78_0/libs/iostreams/src/zlib.cpp"
 #include "../lib/boost_1_78_0/libs/iostreams/src/gzip.cpp"
 #include "gzipWrapper.h"
