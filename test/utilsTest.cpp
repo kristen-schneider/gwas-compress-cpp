ff257b0eb77d88c6e46d257fc412d67f865d2dff
kristen-schneider <kristen.schneider@colorado.edu>
2022-03-02 21:44:36 -0700

    unit testing for split string good for simple cases;

---
+++ test/utilsTest.cpp
@@ -0,0 +1,42 @@
+//
+// Created by Kristen Schneider on 3/2/22.
+//
+
+#include <utils.h>
+#include "gtest/gtest.h"
+#include <vector>
+
+using namespace std;
+
+TEST(SplitStringTest, tab){
+    char delim = '\t';
+    string s = "Hello\tWorld";
+    vector<string> ss = {"Hello", "World"};
+
+    vector<string> test_s = splitString(s, delim);
+    for (int i = 0; i < ss.size(); ++i) {
+        EXPECT_EQ(ss[i], test_s[i]) << "Vectors differ at index " << i;
+    }
+}
+
+TEST(SplitStringTest, space){
+    char delim = ' ';
+    string s = "Hello World";
+    vector<string> ss = {"Hello", "World"};
+
+    vector<string> test_s = splitString(s, delim);
+    for (int i = 0; i < ss.size(); ++i) {
+        EXPECT_EQ(ss[i], test_s[i]) << "Vectors differ at index " << i;
+    }
+}
+
+TEST(SplitStringTest, comma){
+    char delim = ',';
+    string s = "Hello,World";
+    vector<string> ss = {"Hello", "World"};
+
+    vector<string> test_s = splitString(s, delim);
+    for (int i = 0; i < ss.size(); ++i) {
+        EXPECT_EQ(ss[i], test_s[i]) << "Vectors differ at index " << i;
+    }
+}
