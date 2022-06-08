84fb5addfd84121b21ce960ea67aa57a08ef890a
kristen-schneider <kristen.schneider@colorado.edu>
2022-06-08 10:35:39 -0600

    making config file for input parameters

---
+++ src/configReader.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+map read_config_file(string configFile){
+    
+    map<string, string> configurations;
+    return configurations;
+
+}
