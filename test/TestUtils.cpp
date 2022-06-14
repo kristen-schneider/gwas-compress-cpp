006b251db122720df53ab8011b839029c82c9a1b
kristen-schneider <kristen.schneider@colorado.edu>
2022-06-14 11:26:35 -0600

    testing utils files

---
+++ test/TestUtils.cpp
@@ -0,0 +1,18 @@
+#include <cppunit/config/SourcePrefix.h>
+#include "../include/utils.h"
+#include "TestUtils.h"
+
+#include <iostream>
+#include <string>
+
+void
+UtilsTest::setUp(){}
+
+void
+UtilsTest::tearDown(){}
+
+CPPUNIT_TEST_SUITE_REGISTRATION( UtilsTest );
+
+void UtilsTest::testDataType(){
+    CPPUNIT_ASSERT_EQUAL(, 
+}
--- test/TestUtils.h
+++ test/TestUtils.h
@@ -6,4 +6,12 @@
 class UtilsTest : public CPPUNIT_NS::TestFixture{
     CPPUNIT_TEST_SUITE( UtilsTest );
     CPPUNIT_TEST( testDataType );
-}
+    CPPUNIT_TEST_SUITE_END();
+
+    public:
+    void setUp();
+    void tearDown();
+    
+    void testDataType();
+};
+#endif
