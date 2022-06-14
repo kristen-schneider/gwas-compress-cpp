699e9d3be1540bd80d59958f1859caa75ec63057
kristen-schneider <kristen.schneider@colorado.edu>
2022-06-14 11:02:59 -0600

    starting to add more unit tests

---
+++ test/TestUtils.h
@@ -0,0 +1,9 @@
+#ifndef TestUtils_h
+#define TestStudent_h
+
+#include <cppunit/extensions/HelperMacros.h>
+
+class UtilsTest : public CPPUNIT_NS::TestFixture{
+    CPPUNIT_TEST_SUITE( UtilsTest );
+    CPPUNIT_TEST( testDataType );
+}
